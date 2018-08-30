ecde3284e1f53fbdd49f5c32cdc8e19acc1e4eb9
SSJ <njussj@gmail.com>
2018-08-30 11:09:56 +0800

    commit

--- src/main/java/com/nnu/service/RentContactService.java
+++ src/main/java/com/nnu/service/RentContactService.java
@@ -1,6 +1,7 @@
 package com.nnu.service;
 
 import com.nnu.dao.Rent_ContactMapper;
+import com.nnu.domain.Rent_Contact;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +13,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class RentContactService {
     private Rent_ContactMapper rent_contactMapper;
+
+    public void addNewConatctInfo(Rent_Contact rent_contact){
+        rent_contactMapper.insert(rent_contact);
+    }
+
+    public void deleteContactInfo(int contactid){
+        rent_contactMapper.deleteByPrimaryKey(contactid);
+    }
+
+    public void updateContact(Rent_Contact rent_contact){
+        rent_contactMapper.updateByPrimaryKey(rent_contact);
+    }
 
 
     @Autowired
